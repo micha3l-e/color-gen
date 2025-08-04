e317d3e1cce32a9fde84e8171a4d91e2bd1640be
Michael Eggar <128605246+micha3l-e@users.noreply.github.com>
2025-08-03 21:03:03 -0500

    add copy functionality

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 let genBtn = document.getElementById("gen-btn");
 let colorsEl = document.getElementById("colors-el");
-let colorA = document.querySelector(".new-color-a")
-let colorB = document.querySelector(".new-color-b")
+let colorA = document.querySelector(".new-color-a");
+let colorB = document.querySelector(".new-color-b");
+let colorATxt = document.querySelector(".color-a-txt");
+let colorBTxt = document.querySelector(".color-b-txt");
+let allColorTxt = document.querySelectorAll(".color-a-txt, .color-b-txt");
 
 
 function randomColor() {
@@ -12,12 +15,22 @@ function genClick() {
     let c1 = randomColor();
     let c2 = randomColor();
     colorA.style.backgroundColor = c1;
-    colorA.textContent = c1;
+    colorATxt.textContent = c1;
     colorB.style.backgroundColor = c2;
-    colorB.textContent = c2;
-
-
+    colorBTxt.textContent = c2;
+    colorA.style.border = "1px solid #edd9bb";
+    colorB.style.border = "1px solid #edd9bb"
 
 }
 
+function colorClip(h) {
+    const hex = h.currentTarget.textContent;
+    const hexContainer = h.currentTarget;
+    navigator.clipboard.writeText(hex);
+    console.log(hex);
+    hexContainer.setAttribute("data-tip", "Copied!")
+    setTimeout(()=> hexContainer.setAttribute("data-tip", "Copy"), 900)
+}
+
 genBtn.addEventListener('click', genClick);
+allColorTxt.forEach(el => el.addEventListener('click', colorClip));
